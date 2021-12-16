307d1254a942a0cfde3192007c02ba2dab9c7db3
houzairkoussa <houzairmk@icloud.com>
2021-12-16 09:49:01 +0400

    generated tiles board

--- src/App.js
+++ src/App.js
@@ -4,9 +4,9 @@ import Board from "./components/Board";
 
 function App() {
   return (
-    <>
+    <div className='app'>
       <Board />
-    </>
+    </div>
   );
 }
 
--- src/components/Board.js
+++ src/components/Board.js
@@ -4,12 +4,25 @@ class Board extends Component {
   constructor (props) {
     super(props);
 
-    this.state = {};
+    const tiles = [];
+    for (let index = 0; index < 10000; index++) {
+      tiles.push(index);
+    }
+
+    this.state = {
+      tiles: tiles
+    };
   }
 
   render () {
     return (
       <div className="board">
+        { this.state.tiles.map( (tile) =>
+          <div
+            className="tile"
+            key={tile}>
+          </div>
+        )}
       </div>
     );
   }
